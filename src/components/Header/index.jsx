2abebae8e3483c3794fea8397401f306f75a3c5c
IdrisVeli1987 <idrisveliev@gmail.com>
2023-06-16 15:52:11 +0400

    add index.css

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,12 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import MegaTexnikaLogo from "../../assets/logo.png";
 export const Header = () => {
   return (
     <>
-      <Box sx={{ backgroundColor: "#rgba(255, 192, 31, 1)", py: 2}}>
+      <Box sx={{ backgroundColor: "#rgba(255, 192, 31, 1)", py: 2 }}>
         <Container>
           <Stack flexDirection="row" justifyContent="space-between">
             <Typography>
@@ -19,6 +20,32 @@ export const Header = () => {
           </Stack>
         </Container>
       </Box>
+      <Container sx={{ py: 4 }}>
+        <Stack
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <img
+            src={MegaTexnikaLogo}
+            alt="MegaTexnikaLogo"
+            style={{ objectFit: "contain" }}
+          />
+          <Stack flexDirection="row" gap={1}>
+            <Typography component='a' href='#'>Əsas səhifə</Typography>
+            <Typography component='a' href='#'>Haqqımızda</Typography>
+            <Typography component='a' href='#'>Texnikalar</Typography>
+            <Typography component='a' href='#'>İcarə şərtləri</Typography>
+            <Typography component='a' href='#'>Blog</Typography>
+            <Typography component='a' href='#'>Əlaqə</Typography>
+          </Stack>
+          <Stack flexDirection="row" gap={1}>
+            <Typography>AZ</Typography>
+            <Typography>EN</Typography>
+            <Typography>RU</Typography>
+          </Stack>
+        </Stack>
+      </Container>
     </>
   );
 };
